7d87aaa19a6e12260cb01596401db0f0a1227b59
Flappy <stvnsbarrantes@gmail.com>
2025-06-29 13:34:16 +0800

    loacla

--- server_portfolio/server_portfolio/settings.py
+++ server_portfolio/server_portfolio/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env_config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["server-portfolio-m4sq.onrender.com"]
 
 # Application definition
 
