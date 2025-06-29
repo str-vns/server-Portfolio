308b135a3874926403cba387a2e9b096860086ef
Flappy <stvnsbarrantes@gmail.com>
2025-06-29 13:31:29 +0800

    sett

--- server_portfolio/server_portfolio/settings.py
+++ server_portfolio/server_portfolio/settings.py
@@ -28,10 +28,7 @@ SECRET_KEY = env_config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = env_config(
-    "ALLOWED_HOSTS", default=["localhost", "127.0.0.1"], cast=list
-)
-
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
